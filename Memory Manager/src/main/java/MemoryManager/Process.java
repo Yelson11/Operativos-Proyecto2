0b290da5bc28e9a0de6aa30b5dde3f8bc179db0a
arcano50 <47094087+arcano50@users.noreply.github.com>
2019-11-08 04:04:24 -0600

    Update Process.java

--- Memory Manager/src/main/java/MemoryManager/Process.java
+++ Memory Manager/src/main/java/MemoryManager/Process.java
@@ -10,11 +10,18 @@ public class Process
     public ArrayList<Integer> Procesess = new ArrayList();
     
     private int _id;
+    private int _priority;
+    private int _frames;
+    private int _size;
     private String _name;
     
     public Process(String name)
     {
+        SetId();
         _name = name;
+        _size = 0;
+        _priority = 0;
+        _frames = 0;
     }
     
     private void SetId(){
@@ -22,6 +29,14 @@ public class Process
         Procesess.add(_id);
     }
     
-    public int Id(){ return _id; }
+    public int Id() { return _id; }
+    public int Frames() { return _frames; }
+    public int Priority() { return _priority; }
+    public int Size() { return _size; }
     public String Name() { return _name; }
+    
+    public void Frames(int frames) { _frames = frames; }
+    public void Name(String name) { _name = name; }
+    public void Priority(int priority) { _priority = priority; }
+    public void Size(int size) { _size = size; }
 }
